c7d20237ad0dac701d0843279346cbbd26836b72
Rafael Brache <brache.rojas@gmail.com>
2021-04-08 00:34:58 -0400

    addding user view component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
-import UserTable from './components/UserTable';
 // import SearchBar from './components/SearchBar';
-import HomePage from './components/HomePage';
+import HomePage from './components/Home/HomePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import UserList from './components/Users/UserList';
+import UserView from './components/Users/UserView';
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
         <h1>Github Project</h1>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/users' component={UserTable} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/users" component={UserList} />
+        <Route exact path="/users/:username" component={UserView} />
       </div>
     );
   }
