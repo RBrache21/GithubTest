a1c47a397272155fb3dd27f3a4b47d7ce69cb437
Rafael Brache <brache.rojas@gmail.com>
2021-04-07 22:03:06 -0400

    octokit fix

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,4 @@
 import React from 'react';
-import { Octokit } from "@octokit/rest";
-
-const octokit = new Octokit({
-	auth: "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",
-	userAgent: 'gitproject v1.2.3',
-	baseUrl: 'https://api.github.com'
-})
-
-// console.log(octokit.rest.users.list())
 
 const HomePage = () => {
 	return (
--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import Image from 'react-bootstrap/Image'
 import Table from 'react-bootstrap/Table'
-
 import { Octokit } from "@octokit/rest";
 
 const octokit = new Octokit({
-	auth: "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",
+	auth: "",
 	userAgent: 'gitproject v1.2.3',
 	baseUrl: 'https://api.github.com'
 })
 
 const UserTable = () => {
-	const [users, setusers] = useState([]);
+
+	const [users, setUsers] = useState([]);
 	// Fetching data from the github API
 	useEffect(() =>{
+		// axios.get('https://api.github.com/users?&per_page=100')
 		octokit.rest.users.list()
 		.then(res => {
 			console.log(res.data)
-			setusers(res.data)
+			setUsers(res.data)
 		})
 	}, [])
 
